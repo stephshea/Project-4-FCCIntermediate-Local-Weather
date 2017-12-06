ec906a1430c373ea4f723618467209adbefa2c4d
Stephanie Shea <stephcshea@gmail.com>
2017-12-06 02:04:06 +0000

    location working

--- assets/script.js
+++ assets/script.js
@@ -3,14 +3,50 @@
 $(document).ready(function() {
  var long;
  var lat;
+ var tempK;
+ var tempF;
+ var tempC;
+ 
  
   if (navigator.geolocation) {
      navigator.geolocation.getCurrentPosition(function(position) {
       
-      long = position.coords.longitude;
-      lat = position.coords.latitude;
-    $("#data").html("Your latitude: " + lat  + 
+     showWeather(position);
+      console.log(position.coords.longitude);
+      console.log(position.coords.latitude);
+    // $("#data").html("Your latitude: " + lat  + 
 
-    "<br>Your Longitude: " +long);
+    // "<br>Your Longitude: " +long);
+     }
+  )}
+  
+  
+  function showWeather(position) {
+  var long = position.coords.longitude;
+      var lat = position.coords.latitude;
 
-'https://api.openweathermap.org/data/2.5/weather?
+
+$.ajax({
+  method: 'GET',
+  url: 'https://api.openweathermap.org/data/2.5/weather',
+  data: {lat: lat, lon: long, appid:'ae36112ec88d99f153f8a75484b09f50'}, 
+  success: function(yay) {
+  console.log(yay);
+  
+  },
+  
+  
+ });
+ 
+ $.getJSON(function(data){
+  var city;
+  var weatherType;
+  console.log(city);
+$("#city").html(city);
+$("#weatherType").html(weatherType);
+ 
+ });
+ 
+ 
+  }
+}); 
